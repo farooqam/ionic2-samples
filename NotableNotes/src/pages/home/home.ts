5d6f42d8b1542c530e0ce210827205bb5127a534
farooqam <farooq_am@hotmail.com>
2017-12-11 22:52:47 -0800

    Added cards on home page.

--- NotableNotes/src/pages/home/home.ts
+++ NotableNotes/src/pages/home/home.ts
@@ -7,8 +7,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public notes = [
+    {
+      id: '1',
+      date: '2017-12-11',
+      title: 'Ionic 2',
+      content: 'Learn the basics of Ionic 2.'
+    },
+    {
+      id: '2',
+      date: '2017-12-01',
+      title: 'Firebase',
+      content: 'A great backend for Ionic applications.'
+    },
+    {
+      id: '3',
+      date: '2017-12-05',
+      title: 'Angular',
+      content: 'A good grasp of it is crucial to developing great Ionic 2 apps.'
+    }
+  ];
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  public onItemClick(note: any) {
+    console.log('item-click', note);
   }
 
 }
